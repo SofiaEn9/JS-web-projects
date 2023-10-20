28ab2d8efddb83449d0822742ba26ad24adca8c0
SofiaEn9 <sofiaanen9@gmail.com>
2023-10-20 12:04:31 -0600

    Add loading spinner

--- script/1-quote.js
+++ script/1-quote.js
@@ -2,15 +2,28 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const xBtn = document.getElementById("x-btn");
 const newQuoteBtn = document.getElementById("new-quote-btn");
+const quoteContainer = document.getElementById("quote-container");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Loading Spinner Hidden
+function completeLoad() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 // show a new quote
 function newQuote() {
+  loading();
   //pick random quote from apiQutoe array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-
-  quoteText.textContent = `${quote.text}`;
 
   if (!quote.author) {
     authorText.textContent = "-Unknown";
@@ -22,9 +35,12 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  quoteText.textContent = `${quote.text}`;
+  completeLoad();
 }
 
 async function getQuote() {
+  loading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
