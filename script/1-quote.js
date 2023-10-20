29060874448df0bacd08580b3cc477340d8f55af
SofiaEn9 <sofiaanen9@gmail.com>
2023-10-20 11:38:57 -0600

    Manipulate DOM with JS

--- script/1-quote.js
+++ script/1-quote.js
@@ -1,10 +1,27 @@
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const xBtn = document.getElementById("x-btn");
+const newQuoteBtn = document.getElementById("new-quote-btn");
+
 let apiQuotes = [];
 
 // show a new quote
 function newQuote() {
   //pick random quote from apiQutoe array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  quoteText.textContent = `${quote.text}`;
+
+  if (!quote.author) {
+    authorText.textContent = "-Unknown";
+  } else {
+    authorText.textContent = `-${quote.author}`;
+  }
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
 }
 
 async function getQuote() {
@@ -17,6 +34,15 @@ async function getQuote() {
     //catch error here
   }
 }
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+xBtn.addEventListener("click", tweetQuote);
+newQuoteBtn.addEventListener("click", newQuote);
 
 // On load
 getQuote();
