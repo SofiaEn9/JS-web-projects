00005687696b0cb616edea2ab67907afd1304b76
SofiaEn9 <sofiaanen9@gmail.com>
2023-10-23 15:22:47 -0600

    Display fetched images

--- script/2-scroll.js
+++ script/2-scroll.js
@@ -1,18 +1,47 @@
-photoArray = [];
+const imgContainer = document.getElementById("img-container");
+const loader = document.getElementById("loader");
+
+let photoArray = [];
+
+// Unsplash API
+const apiKey = "TLNxFEDopcl6V9fWExgVhguj33_9Qo5dNo0_0ehdcGM";
+const count = 10;
+const collection = "nature";
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&collection=${collection}`;
+
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  photoArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    item.appendChild(img);
+    imgContainer.appendChild(item);
+  });
+}
 
 async function getPhotos() {
-  const apiKey = "TLNxFEDopcl6V9fWExgVhguj33_9Qo5dNo0_0ehdcGM";
-  const count = 10;
-  const collection = "nature";
-  const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&collection=${collection}`;
   try {
     const response = await fetch(apiUrl);
-    const photoArray = await response.json();
+    photoArray = await response.json();
     console.log(photoArray);
+    displayPhotos();
   } catch (error) {
     //Catch error here
   }
 }
 
-// On load
 getPhotos();
