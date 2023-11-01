828fc2dbae8a1808037abca4ea5bec9efe16c5cb
SofiaEn9 <sofiaanen9@gmail.com>
2023-11-01 13:48:27 -0600

    Add play audio function

---
+++ script/7-music-player.js
@@ -0,0 +1,23 @@
+const music = document.querySelector("audio");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const fwdBtn = document.getElementById("fwd");
+
+// Check if playing
+let isPlaying = false;
+
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
