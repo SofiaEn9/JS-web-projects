9d0de27e32417332be7586727851f13dcb971522
SofiaEn9 <sofiaanen9@gmail.com>
2023-11-08 13:54:37 -0600

    Set environment for countdown completion

--- script/8-countdown.js
+++ script/8-countdown.js
@@ -1,10 +1,15 @@
 const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
-const datePicker = document.getElementById("date-picker");
-const countdownContainer = document.getElementById("countdown");
+const dateElement = document.getElementById("date-picker");
+
+const countdownContainer = document.getElementById("countdown-container");
 const countdownElementTitle = document.getElementById("countdown-title");
 const timeElements = document.querySelectorAll("span");
-const countdownBtn = document.getElementById("submit-btn");
+const countdownBtn = document.getElementById("reset-btn");
+
+const completeContainer = document.getElementById("complete-container");
+const completeElementInfo = document.getElementById("complete-info");
+const completeBtn = document.getElementById("complete-btn");
 
 let countdownTitle = "";
 let countdownDate = "";
@@ -18,7 +23,7 @@ const day = hr * 24;
 
 // Add a min range to date picker
 const today = new Date().toISOString().split("T")[0];
-datePicker.setAttribute("min", today);
+dateElement.setAttribute("min", today);
 
 function updateDOM() {
   countdownActive = setInterval(() => {
@@ -32,14 +37,22 @@ function updateDOM() {
     console.log(days, hrs, mins, secs);
     // Hide Input Container
     inputContainer.hidden = true;
-    // Show Countdown Container
-    countdownContainer.hidden = false;
-    // Populate Countdown
-    countdownElementTitle.textContent = `${countdownTitle}`;
-    timeElements[0].textContent = `${days}`;
-    timeElements[1].textContent = `${hrs}`;
-    timeElements[2].textContent = `${mins}`;
-    timeElements[3].textContent = `${secs}`;
+
+    if (distance < 0) {
+      clearInterval(countdownActive);
+      completeElementInfo.textContent = `${countdownTitle} completed on ${countdownDate}`;
+      countdownContainer.hidden = true;
+      completeContainer.hidden = false;
+    } else {
+      // Show Countdown Container
+      countdownContainer.hidden = false;
+      // Populate Countdown
+      countdownElementTitle.textContent = `${countdownTitle}`;
+      timeElements[0].textContent = `${days}`;
+      timeElements[1].textContent = `${hrs}`;
+      timeElements[2].textContent = `${mins}`;
+      timeElements[3].textContent = `${secs}`;
+    }
   }, sec);
 }
 
@@ -61,6 +74,7 @@ function updateCountdown(event) {
 function resetCoutdown() {
   // Show Input Container / Hide Countdown
   countdownContainer.hidden = true;
+  completeContainer.hidden = true;
   inputContainer.hidden = false;
   // Stop Countdown
   clearInterval(countdownActive);
@@ -71,3 +85,4 @@ function resetCoutdown() {
 
 countdownForm.addEventListener("submit", updateCountdown);
 countdownBtn.addEventListener("click", resetCoutdown);
+completeBtn.addEventListener("click", resetCoutdown);
