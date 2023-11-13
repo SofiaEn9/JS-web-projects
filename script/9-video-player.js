8a4e34701a78a6d6f1ac197f4a492934a7e3f10b
SofiaEn9 <sofiaanen9@gmail.com>
2023-11-13 13:47:58 -0600

    Add functionality to Progress Bar

--- script/9-video-player.js
+++ script/9-video-player.js
@@ -26,6 +26,29 @@ function togglePlay() {
   }
 }
 
+// Progress Bar
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${video.duration}`;
+}
+
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+
+function setProgress(event) {
+  const newTime = event.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+}
+
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 video.addEventListener("ended", showPlayIcon);
+video.addEventListener("canplay", updateProgress);
+video.addEventListener("timeupdate", updateProgress);
+progressRange.addEventListener("click", setProgress);
