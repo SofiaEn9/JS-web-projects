37302a4427e8a8d5b52ad1093931a0587c53c257
SofiaEn9 <sofiaanen9@gmail.com>
2023-11-20 12:55:40 -0600

    Add operator functionality

--- script/12-calculator.js
+++ script/12-calculator.js
@@ -2,31 +2,58 @@ const inputBtns = document.querySelectorAll("button");
 const calculatorDisplay = document.querySelector("h1");
 const clearBtn = document.getElementById("clear-btn");
 
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
+function sendNumberValue(number) {
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
+}
+
 function addDecimal() {
+  // if operator pressed, don't add decimal
+  if (awaitingNextValue) return;
+  // if there is no decimal, add one
   if (!calculatorDisplay.textContent.includes(".")) {
     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   }
 }
 
-function sendNumberValue(number) {
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent =
-    displayValue === "0" ? number : displayValue + number;
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  if (operatorValue && awaitingNextValue) return;
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    console.log(firstValue, operatorValue, currentValue);
+  }
+  awaitingNextValue = true;
+  operatorValue = operator;
+}
+
+// Reset Display
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
 }
 
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("operator")) {
-    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener("click", () => useOperator(inputBtn.value));
   } else if (inputBtn.classList.contains("decimal")) {
     inputBtn.addEventListener("click", () => addDecimal());
   }
 });
-
-// Reset Display
-function resetAll() {
-  calculatorDisplay.textContent = "0";
-}
 
 clearBtn.addEventListener("click", resetAll);
