6077f7b20a6402f84e3f8a6ece54b68db4399f90
SofiaEn9 <sofiaanen9@gmail.com>
2023-11-20 13:05:53 -0600

    Add functionality to calculation

--- script/12-calculator.js
+++ script/12-calculator.js
@@ -26,14 +26,30 @@ function addDecimal() {
   }
 }
 
+const calculate = {
+  "/": (firstNumber, secondNumber) => firstNumber / secondNumber,
+  "*": (firstNumber, secondNumber) => firstNumber * secondNumber,
+  "+": (firstNumber, secondNumber) => firstNumber + secondNumber,
+  "-": (firstNumber, secondNumber) => firstNumber - secondNumber,
+  "=": (firstNumber, secondNumber) => secondNumber,
+};
+
 function useOperator(operator) {
   const currentValue = Number(calculatorDisplay.textContent);
-  if (operatorValue && awaitingNextValue) return;
+  // Prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+    return;
+  }
+  // Assign firstValue if no value
   if (!firstValue) {
     firstValue = currentValue;
   } else {
-    console.log(firstValue, operatorValue, currentValue);
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
   }
+  // Ready for next value, store operator
   awaitingNextValue = true;
   operatorValue = operator;
 }
