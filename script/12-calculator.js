30ea6c97ee961b187fd8b69ec019df0e34fc439a
SofiaEn9 <sofiaanen9@gmail.com>
2023-11-20 11:43:22 -0600

    Add functionality to Reset and Decimal buttons

--- script/12-calculator.js
+++ script/12-calculator.js
@@ -2,6 +2,12 @@ const inputBtns = document.querySelectorAll("button");
 const calculatorDisplay = document.querySelector("h1");
 const clearBtn = document.getElementById("clear-btn");
 
+function addDecimal() {
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
 function sendNumberValue(number) {
   const displayValue = calculatorDisplay.textContent;
   calculatorDisplay.textContent =
@@ -14,6 +20,13 @@ inputBtns.forEach((inputBtn) => {
   } else if (inputBtn.classList.contains("operator")) {
     inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("decimal")) {
-    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener("click", () => addDecimal());
   }
 });
+
+// Reset Display
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+}
+
+clearBtn.addEventListener("click", resetAll);
