976e80243aa20a5f33764415b6b98873680829d4
SofiaEn9 <sofiaanen9@gmail.com>
2023-12-15 12:52:14 -0600

    Add Tabbed component

--- script/6-bankist.js
+++ script/6-bankist.js
@@ -10,6 +10,9 @@ const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 const navBar = document.querySelector(".nav__links");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -47,7 +50,6 @@ btnScrollTo.addEventListener("click", linkScrollTo);
 
 function navScrollTo(e) {
   e.preventDefault();
-  console.log(e.target);
   if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
@@ -55,6 +57,27 @@ function navScrollTo(e) {
 }
 
 navBar.addEventListener("click", navScrollTo);
+
+// Tabbed Component
+function changeTab(e) {
+  const clickedButton = e.target.closest(".operations__tab");
+  // Guard clause
+  if (!clickedButton) return;
+  // Remove active classes
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  tabsContent.forEach((content) =>
+    content.classList.remove("operations__content--active")
+  );
+  // Activate tab
+  clickedButton.classList.add("operations__tab--active");
+  // Activate content area
+
+  document
+    .querySelector(`.operations__content--${clickedButton.dataset.tab}`)
+    .classList.add("operations__content--active");
+}
+// tabs.forEach(tab => tab.addEventListener("click", changeTab);
+tabsContainer.addEventListener("click", changeTab);
 
 // document.querySelectorAll('.nav__link').forEach(function (el)) {
 //   el.addEventListener('click', )
